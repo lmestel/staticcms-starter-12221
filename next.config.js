da151d78dbed75328db2f2d0f75f4451c3fc3b90
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-02-11 14:58:23 +0100

    feature: initial commit

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    "@kickstartds/base",
+    "@kickstartds/blog",
+    "@kickstartds/core",
+    "@kickstartds/form",
+    "@kickstartds/ds-agency",
+  ],
+};
+
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+    });
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+  ...nextConfig,
+};
